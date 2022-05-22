f25034e094560fce780fafe0578dd9b19a2a63eb
Nathan Lafferty <nslafferty@gmail.com>
2022-05-22 16:00:38 -0500

    scaffold admin interface

---
+++ components/admin_controller.mjs
@@ -0,0 +1,25 @@
+import {html, render} from 'https://unpkg.com/lit-html?module';
+import {RSVPBackend} from '/components/backend.mjs';
+import {WeddingHeader} from '/components/wedding_header.mjs';
+
+class AdminController {
+
+	async HandleDeleteData(e) {
+		e.preventDefault();
+		let rsvpBackend = new RSVPBackend();
+		await rsvpBackend.DeleteAllData();
+	}
+	async HandleUploadData(e) {
+		e.preventDefault();
+		let rsvpBackend = new RSVPBackend();
+		await rsvpBackend.UploadRSVPData();
+	}
+
+	async init() {
+		document.getElementById("load_data").addEventListener("click", this.HandleUploadData.bind(this));
+		document.getElementById("delete_data").addEventListener("click", this.HandleDeleteData.bind(this));
+	}
+}
+
+const controller = new AdminController();
+controller.init();
